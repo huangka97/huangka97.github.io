f50dccb293aec8403ba2a532fd84517618c1a9e2
Karl Huang <karl.huang2009@gmail.com>
2018-10-20 16:05:23 -0700

    fourth  attempt at new implemented design

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Ismail Tasdelen.", "Security Researcher.", "Bug Bounty Hunter.", "Developer."],
+      strings: ["Karl Huang.", "Student.", "Developer.", "Foodie."],
       typeSpeed: 100,
       loop: true,
     });
